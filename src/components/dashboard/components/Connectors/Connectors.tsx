5ca31511111d314d0a2992a1d456f094af631663
dennykjohn <denny.john.mail@gmail.com>
2025-07-02 12:44:06 +0530

    Components page

--- src/components/dashboard/components/Connectors/Connectors.tsx
+++ src/components/dashboard/components/Connectors/Connectors.tsx
@@ -1,17 +1,27 @@
 import { Flex } from "@chakra-ui/react";
 
+import ClientRoutes from "@/constants/client-routes";
+
+import PageHeader from "../../wrapper/PageHeader";
+
 const Connectors = () => {
   return (
-    <Flex
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      height="100%"
-      width="100%"
-      padding={4}
-    >
-      <h1>Connectors Page</h1>
-      <p>This is where you can manage your connectors.</p>
+    <Flex flexDirection="column" gap={8}>
+      <PageHeader
+        breadcrumbs={[
+          {
+            label: "Connectors",
+            route: `${ClientRoutes.DASHBOARD}/${ClientRoutes.CONNECTORS}`,
+          },
+          { label: "Connectors", route: "" },
+        ]}
+        buttonLabel="Add Connector"
+        onCreateClick={() => console.log("Add Connector clicked")}
+      />
+      <Flex>
+        <h1>Connectors Page</h1>
+        <p>This is where you can manage your connectors.</p>
+      </Flex>
     </Flex>
   );
 };
---
+++ src/components/dashboard/wrapper/PageHeader.tsx
@@ -0,0 +1,55 @@
+import { ReactNode } from "react";
+
+import { Breadcrumb, Button, Flex } from "@chakra-ui/react";
+
+import { FaPlus } from "react-icons/fa6";
+
+import { useNavigate } from "react-router";
+
+interface PageHeaderProps {
+  breadcrumbs: { label: string; route?: string }[];
+  buttonLabel: string;
+  onCreateClick: () => void;
+  children?: ReactNode;
+}
+
+const PageHeader = ({
+  breadcrumbs,
+  buttonLabel,
+  onCreateClick,
+  children,
+}: PageHeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleBreadcrumbClick = (route?: string) => {
+    if (route) {
+      navigate(route);
+    }
+  };
+
+  return (
+    <Flex justifyContent="space-between" align="center" w="100%">
+      <Flex direction="column" gap={2}>
+        <Breadcrumb.Root size="lg">
+          <Breadcrumb.List>
+            {breadcrumbs.map(({ route, label }, idx) => (
+              <Breadcrumb.Item key={idx} cursor={route ? "pointer" : "default"}>
+                {idx > 0 && <Breadcrumb.Separator />}
+                <Breadcrumb.Link onClick={() => handleBreadcrumbClick(route)}>
+                  {label}
+                </Breadcrumb.Link>
+              </Breadcrumb.Item>
+            ))}
+          </Breadcrumb.List>
+        </Breadcrumb.Root>
+        {children}
+      </Flex>
+      <Button colorPalette="brand" onClick={onCreateClick}>
+        <FaPlus />
+        {buttonLabel}
+      </Button>
+    </Flex>
+  );
+};
+
+export default PageHeader;
