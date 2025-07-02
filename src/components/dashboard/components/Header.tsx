e96a52a2eed364638ea457c7540a950718c398fe
dennykjohn <denny.john.mail@gmail.com>
2025-07-02 12:07:06 +0530

    add avatar in header

--- src/components/dashboard/components/Header.tsx
+++ src/components/dashboard/components/Header.tsx
@@ -1,6 +1,11 @@
-import { Box, GridItem, useBreakpointValue } from "@chakra-ui/react";
-
-import { LuPanelLeftClose } from "react-icons/lu";
+import {
+  Avatar,
+  Flex,
+  GridItem,
+  Menu,
+  Portal,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 const Header = ({ sidebarWidth }: { sidebarWidth: string }) => {
   const sidebarDisplay = useBreakpointValue({ base: "none", md: "block" });
@@ -15,14 +20,40 @@ const Header = ({ sidebarWidth }: { sidebarWidth: string }) => {
       right={0}
       height="60px"
       zIndex="999"
-      pl={6}
+      paddingInline={6}
       display="flex"
       alignItems="center"
       boxShadow={"0 2px 4px rgba(0, 0, 0, 0.1)"}
     >
-      <Box color="gray.500">
-        <LuPanelLeftClose size={24} cursor="pointer" />
-      </Box>
+      <Flex
+        marginLeft="auto"
+        justifyContent="space-between"
+        alignItems={"center"}
+      >
+        <Menu.Root positioning={{ placement: "bottom-end" }}>
+          <Menu.Trigger rounded="full">
+            <Avatar.Root colorPalette="blue" cursor="pointer">
+              <Avatar.Fallback name="Random" />
+              <Avatar.Image src="https://randomuser.me/api/portraits/men/42.jpg" />
+            </Avatar.Root>
+          </Menu.Trigger>
+          <Portal>
+            <Menu.Positioner>
+              <Menu.Content>
+                <Menu.Item value="account" cursor="pointer">
+                  Account
+                </Menu.Item>
+                <Menu.Item value="settings" cursor="pointer">
+                  Settings
+                </Menu.Item>
+                <Menu.Item value="logout" cursor="pointer">
+                  Logout
+                </Menu.Item>
+              </Menu.Content>
+            </Menu.Positioner>
+          </Portal>
+        </Menu.Root>
+      </Flex>
     </GridItem>
   );
 };
