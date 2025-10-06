036dd158b5d832e612006ba6aff976b6d3ad8e03
dennykjohn <denny.john.mail@gmail.com>
2025-10-06 12:14:17 +0530

    Fix add shcema logic

--- src/components/dashboard/components/Connectors/components/ConnectorDetails/components/Tabs/Schema/Schema.tsx
+++ src/components/dashboard/components/Connectors/components/ConnectorDetails/components/Tabs/Schema/Schema.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 import {
   ActionBar,
@@ -38,9 +38,8 @@ import Actions from "./Actions";
 
 const Schema = () => {
   const context = useOutletContext<Connector>();
-  const { data: tables, isLoading } = useFetchConnectorTableById(
-    context.connection_id,
-  );
+  const { data: AllTableList, isLoading: isAllTableListLoading } =
+    useFetchConnectorTableById(context.connection_id);
   const { data: SelectedTables, isLoading: isLoadingSelected } =
     useFetchSelectedTables(context.connection_id);
   const { mutate: updateTables, isPending: isAssigningTables } =
@@ -54,6 +53,26 @@ const Schema = () => {
   >([]);
 
   const [checkedTables, setCheckedTables] = useState<ConnectorTable[]>([]);
+  const [copyOfInitialCheckedTables, setCopyOfInitialCheckedTables] = useState<
+    ConnectorTable[]
+  >([]);
+
+  // 1. Store initial checkedTables on first load
+  useEffect(() => {
+    if (AllTableList && SelectedTables) {
+      const checked = AllTableList.filter((t) => t.selected);
+      setCopyOfInitialCheckedTables(checked);
+    }
+  }, [AllTableList, SelectedTables]);
+
+  // Show Action bar when if the contents of
+  // checked table and copyOfInitialCheckedTables changes
+  const hasCheckedTablesChanged =
+    checkedTables.length !== copyOfInitialCheckedTables.length ||
+    checkedTables.some(
+      (table) =>
+        !copyOfInitialCheckedTables.find((t) => t.table === table.table),
+    );
 
   useEffect(() => {
     if (SelectedTables) {
@@ -61,10 +80,15 @@ const Schema = () => {
     }
   }, [SelectedTables]);
 
-  const unSelectedTables = useMemo(
-    () => tables?.filter((t) => !t.selected) || [],
-    [tables],
-  );
+  // Set selected Tables in AllTableList in CheckedTables
+  useEffect(() => {
+    if (AllTableList) {
+      const checked = AllTableList.filter((t) =>
+        selectedTables.some((st) => st.table === t.table),
+      );
+      setCheckedTables(checked);
+    }
+  }, [AllTableList, selectedTables]);
 
   // Track expanded tables
   const toggleExpand = (table: string) =>
@@ -123,7 +147,7 @@ const Schema = () => {
     );
   };
 
-  if (isLoading || isLoadingSelected) {
+  if (isAllTableListLoading || isLoadingSelected) {
     return <LoadingSpinner />;
   }
 
@@ -148,69 +172,72 @@ const Schema = () => {
               Select
             </Text>
           </Flex>
-          {unSelectedTables?.map((item, index) => {
-            const { table, table_fields } = item;
-            const isEven = index % 2 === 0;
-            const rowBg = isEven ? "gray.100" : "white";
-            const isExpanded = !!expanded[table];
+          {(isAssigningTables || isAllTableListLoading) && <LoadingSpinner />}
+          {!isAssigningTables &&
+            AllTableList?.map((item, index) => {
+              const { table, table_fields } = item;
+              const isEven = index % 2 === 0;
+              const rowBg = isEven ? "gray.100" : "white";
+              const isExpanded = !!expanded[table];
 
-            return (
-              <Flex
-                key={table}
-                justifyContent="space-between"
-                backgroundColor={rowBg}
-                alignItems="center"
-                direction={isExpanded ? "column" : "row"}
-                padding={2}
-                borderRadius={4}
-              >
-                <Flex alignItems="center" gap={2} width="100%">
-                  <Box
-                    onClick={() => toggleExpand(table)}
-                    style={{ cursor: "pointer" }}
-                    padding={1}
-                    _hover={{ backgroundColor: "brand.200", borderRadius: 4 }}
-                  >
-                    {isExpanded ? <IoCaretDownSharp /> : <IoMdPlay />}
-                  </Box>
-                  <Text
-                    fontSize="sm"
-                    onClick={() => toggleExpand(table)}
-                    style={{ cursor: "pointer" }}
-                  >
-                    {table}
-                  </Text>
-                  <Checkbox.Root
-                    colorPalette="brand"
-                    marginLeft="auto"
-                    variant="solid"
-                    onCheckedChange={({ checked }) => {
-                      if (checked) {
-                        setCheckedTables((prev) => [...prev, item]);
-                      } else {
-                        setCheckedTables((prev) =>
-                          prev.filter((t) => t.table !== table),
-                        );
-                      }
-                    }}
-                  >
-                    <Checkbox.HiddenInput />
-                    <Checkbox.Control cursor="pointer" />
-                  </Checkbox.Root>
-                </Flex>
-                {isExpanded && (
-                  <Flex direction="column" gap={2} paddingBlock={4}>
-                    {table_fields &&
-                      Object.entries(table_fields).map(([field, type]) => (
-                        <Text key={field} fontSize="sm">
-                          {field}: {type}
-                        </Text>
-                      ))}
+              return (
+                <Flex
+                  key={table}
+                  justifyContent="space-between"
+                  backgroundColor={rowBg}
+                  alignItems="center"
+                  direction={isExpanded ? "column" : "row"}
+                  padding={2}
+                  borderRadius={4}
+                >
+                  <Flex alignItems="center" gap={2} width="100%">
+                    <Box
+                      onClick={() => toggleExpand(table)}
+                      style={{ cursor: "pointer" }}
+                      padding={1}
+                      _hover={{ backgroundColor: "brand.200", borderRadius: 4 }}
+                    >
+                      {isExpanded ? <IoCaretDownSharp /> : <IoMdPlay />}
+                    </Box>
+                    <Text
+                      fontSize="sm"
+                      onClick={() => toggleExpand(table)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {table}
+                    </Text>
+                    <Checkbox.Root
+                      colorPalette="brand"
+                      marginLeft="auto"
+                      variant="solid"
+                      onCheckedChange={({ checked }) => {
+                        if (checked) {
+                          setCheckedTables((prev) => [...prev, item]);
+                        } else {
+                          setCheckedTables((prev) =>
+                            prev.filter((t) => t.table !== table),
+                          );
+                        }
+                      }}
+                      checked={checkedTables.some((t) => t.table === table)}
+                    >
+                      <Checkbox.HiddenInput />
+                      <Checkbox.Control cursor="pointer" />
+                    </Checkbox.Root>
                   </Flex>
-                )}
-              </Flex>
-            );
-          })}
+                  {isExpanded && (
+                    <Flex direction="column" gap={2} paddingBlock={4}>
+                      {table_fields &&
+                        Object.entries(table_fields).map(([field, type]) => (
+                          <Text key={field} fontSize="sm">
+                            {field}: {type}
+                          </Text>
+                        ))}
+                    </Flex>
+                  )}
+                </Flex>
+              );
+            })}
         </Flex>
         <Flex
           direction="column"
@@ -252,21 +279,30 @@ const Schema = () => {
                 </Flex>
                 <Flex gap={3}>
                   <Box
-                    _hover={{ color: "brand.500", backgroundColor: "gray.300" }}
+                    _hover={{
+                      color: "brand.500",
+                      backgroundColor: "gray.300",
+                    }}
                     p={1}
                     borderRadius="sm"
                   >
                     <IoRefreshSharp cursor="pointer" />
                   </Box>
                   <Box
-                    _hover={{ color: "brand.500", backgroundColor: "gray.300" }}
+                    _hover={{
+                      color: "brand.500",
+                      backgroundColor: "gray.300",
+                    }}
                     p={1}
                     borderRadius="sm"
                   >
                     <SlRefresh cursor="pointer" />
                   </Box>
                   <Box
-                    _hover={{ color: "brand.500", backgroundColor: "gray.300" }}
+                    _hover={{
+                      color: "brand.500",
+                      backgroundColor: "gray.300",
+                    }}
                     p={1}
                     borderRadius="sm"
                   >
@@ -278,14 +314,10 @@ const Schema = () => {
           })}
         </Flex>
       </Grid>
-      <ActionBar.Root open={checkedTables.length > 0}>
+      <ActionBar.Root open={hasCheckedTablesChanged}>
         <Portal>
           <ActionBar.Positioner>
             <ActionBar.Content>
-              <ActionBar.SelectionTrigger>
-                {checkedTables.length} selected
-              </ActionBar.SelectionTrigger>
-              <ActionBar.Separator />
               <Button
                 variant="outline"
                 size="sm"
@@ -293,7 +325,7 @@ const Schema = () => {
                 loading={isAssigningTables}
               >
                 <GoPlus />
-                Add
+                Save
               </Button>
             </ActionBar.Content>
           </ActionBar.Positioner>
