1c39fdf7d16411538dd45d16983a059590e75f73
Annmary Benny <annmarybenny985@gmail.com>
2025-11-24 11:29:03 +0530

    sync_progress

--- src/components/dashboard/helpers/DynamicForm.tsx
+++ src/components/dashboard/helpers/DynamicForm.tsx
@@ -161,7 +161,7 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
             name={field.name}
             value={values[field.name]}
             onChange={handleChange}
-            placeholder={`Enter ${field.label}`}
+            placeholder={`Enter ${field.label.toLowerCase()}`}
           />
           {errors[field.name] && (
             <Field.ErrorText>{errors[field.name]}</Field.ErrorText>
@@ -183,7 +183,7 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
           type={inputType}
           value={values[field.name]}
           onChange={handleChange}
-          placeholder={`Enter ${field.label}`}
+          placeholder={`Enter ${field.label}`.toLowerCase()}
         />
         {errors[field.name] && (
           <Field.ErrorText>{errors[field.name]}</Field.ErrorText>
