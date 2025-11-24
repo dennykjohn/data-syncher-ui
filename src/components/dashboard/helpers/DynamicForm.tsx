fdf41d2690b9da4a8a20ab4ffc12a6f6be654f20
Annmary Benny <annmarybenny985@gmail.com>
2025-11-24 11:33:24 +0530

    sync_progress

--- src/components/dashboard/helpers/DynamicForm.tsx
+++ src/components/dashboard/helpers/DynamicForm.tsx
@@ -48,7 +48,7 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
   const [values, setValues] = useState<Record<string, string>>(initialValues);
   const [errors, setErrors] = useState<Record<string, string>>({});
 
-  // when defaultValues changes (edit mode), update state
+  //👇 when defaultValues changes (edit mode), update state
   useEffect(() => {
     if (defaultValues) {
       setValues((prev) => ({
@@ -183,7 +183,7 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
           type={inputType}
           value={values[field.name]}
           onChange={handleChange}
-          placeholder={`Enter ${field.label}`.toLowerCase()}
+          placeholder={`Enter ${field.label.toLowerCase()}`}
         />
         {errors[field.name] && (
           <Field.ErrorText>{errors[field.name]}</Field.ErrorText>
