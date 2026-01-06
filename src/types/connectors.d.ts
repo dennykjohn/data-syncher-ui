8d1593e4c2a2771029eeaf0fcc01e7dfefb40c4c
Annmary Benny <annmarybenny985@gmail.com>
2026-01-06 12:23:28 +0530

    modifications

--- src/types/connectors.d.ts
+++ src/types/connectors.d.ts
@@ -91,7 +91,7 @@ export type ConnectorSettingsApiResponse = {
 export type ConnectorTable = {
   table: string;
   selected: boolean;
-  sequence: null;
+  sequence: number | null;
   is_delta: boolean;
   table_fields: Record<string, string>;
 };
@@ -100,7 +100,7 @@ export interface ConnectorSelectedTable {
   tbl_id: number;
   table: string;
   sequence: number;
-  status: "in_progress" | "completed" | "failed";
+  status: "in_progress" | "completed" | "failed" | null;
 }
 
 // ------------------ Connector Activity Types ------------------
